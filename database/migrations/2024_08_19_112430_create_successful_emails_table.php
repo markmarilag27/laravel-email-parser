fbccf367349c2287783052341fb401f64c68170c
Mark Marilag <mark.marilag27@gmail.com>
2024-08-21 00:12:10 +0800

    fix: migration existing table

--- database/migrations/2024_08_19_112430_create_successful_emails_table.php
+++ database/migrations/2024_08_19_112430_create_successful_emails_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('successful_emails')) {
+            return;
+        }
+
         Schema::create('successful_emails', function (Blueprint $table) {
             $table->id();
             $table->mediumInteger('affiliate_id')->index();
